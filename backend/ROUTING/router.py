50d3436b0fad05c42ea92c858ce52e5135179e70
Ali Azeem <aliazeem3007@gmail.com>
2025-04-25 21:00:42 +0800

    ammar paksa

---
+++ backend/ROUTING/router.py
@@ -0,0 +1,8 @@
+#input is json containing coordinates of points required to be travelled 
+#output is google map link giving the multi stop route, according to the least cost 
+
+import 
+class router:
+    def __init__(self)
+        pass
+    def generate_multi_stop_gmap_url(self, coo)
